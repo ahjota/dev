8cae4ce657aa5cf956602225631f4a8c64cb9d34
AJ Alon <alexander.joel.alon@gmail.com>
2025-07-05 22:56:13 -0700

    remove git-config

--- config/git-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git config --global pull.rebase true
-git config --global push.default current
-if [ -x "$(command -v code)" ]; then
-        git config --global core.editor "code --wait"
-else
-        git config --global core.editor vim
-fi
